df9c85c0e2cc182e575c8ca73d129b806153e26c
marcosemilson <marcosenpereira@gmail.com>
2024-10-12 17:34:15 -0300

    teste

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "BancodeEmprego": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
